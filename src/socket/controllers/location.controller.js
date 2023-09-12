e29b1e9ecebef272258a056399aaf862a7c05983
Keng Zone Store <kengzonestore@iMac-cua-Keng.local>
2023-09-12 09:57:49 +0700

    feat/newProject

--- src/socket/controllers/location.controller.js
+++
@@ -1,5 +0,0 @@
-
-const { pickupPiece } = require('../services/location.service');
-module.exports = {
-    pickupPiece: (socket) => socket.on('pickup-piece', pickupPiece(socket))
-}
--- src/socket/services/index.service.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getStudent: (socket) => {
-        return async (...data) => {
-            console.log(">>> check ", data);
-            socket.emit('mess', { mess: 'Hello' })
-        }
-    }
-}
